a382b5556df646637b4f4d75e4da1bc16265218e
Nikunj <Nikunj1813011@gmail.com>
2021-05-09 11:10:44 -0700

    initcom

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import CurrentBalance from './currentblanace';
+import Showdetails from './Showdetail';
+import History from './History';
+import AddTransaction from './Addtransaction';
+import firebase from "firebase/app";
+import "firebase/performance";
+import "firebase/firestore";
+import { BrowserRouter, Route,useParams } from "react-router-dom";
+
+let expense1=0;
+  let income1=0;
+  
+  let arr=[];
+  let total=0;
+let count=0;
+  const App =()=> {
+  const [alldata,setdata]=React.useState([]);
+  const [alerterror,setError]=React.useState("");  
+  const { name }=useParams();
+  React.useEffect(()=>{
+    console.log("hii ine the useEffect");
+    firebase.firestore().collection('Expenses').where("name","==",name).onSnapshot((snapshot)=>{
+        console.log(snapshot+"hii");
+    arr=[];
+    expense1=0;
+    income1=0; 
+        snapshot.docs.map((doc)=>{
+          let data=doc.data();
+          data['id']=doc.id;
+            arr.push(data);
+            expense1+=parseInt(doc.data().Expense);
+            income1+=parseInt(doc.data().income);
+        
+      })
+      total=parseInt(income1)+parseInt(expense1);
+    
+      setdata(arr);
+    count=count+1;
+//console.log(count+" "+"times");
+      })
+  },[]) 
+
+ 
+
+
+
+  
+
+    return (
+    <div className="App">
+      <Navbar/>
+      <CurrentBalance balance={total} />
+      <Showdetails income={income1} expense={expense1}/>
+      <h3 style={{marginBottom:0}} >Your History</h3>
+      <img src="https://miro.medium.com/max/3288/1*QWL8FlOH8HACzfcMW6K8QA.png" width='210'/>
+      <History history={arr} />
+      <AddTransaction name={name}/>
+    </div>
+    )
+
+  }
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Signup from "./signup"; 
+import Login from "./Login";
+import { BrowserRouter, Route, Link,Switch } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/performance";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAY7wejzkQFByzDumPIGl7LjdYSIWIYMJc",
+  authDomain: "expense-78336.firebaseapp.com",
+  projectId: "expense-78336",
+  storageBucket: "expense-78336.appspot.com",
+  messagingSenderId: "215432279540",
+  appId: "1:215432279540:web:a46260126925961e29b5a1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(
+  <React.StrictMode>
+  <BrowserRouter>
+<Switch>
+
+  <Route exact path="/signup" exact component={Signup}></Route>
+<Route exact path="/expenseapp/:name" exact component={App}></Route>
+<Route exact path="/" exact component={Login}></Route>
+<Route component={Login} />
+</Switch>
+</BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
